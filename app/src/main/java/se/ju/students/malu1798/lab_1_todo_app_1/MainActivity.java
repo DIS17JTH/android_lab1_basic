e819351903697cd2d79094c51ee1f97e69394dbb
malu1798 <malu1798@student.ju.se>
2019-01-14 19:11:52 +0100

    Initial commit

---
+++ app/src/main/java/se/ju/students/malu1798/lab_1_todo_app_1/MainActivity.java
@@ -0,0 +1,13 @@
+package se.ju.students.malu1798.lab_1_todo_app_1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
